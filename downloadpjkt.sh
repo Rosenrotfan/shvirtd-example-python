81628ee181310b54537d6350fed62acde3f1eba0
Анатолий Петров <rosenrotfan@gmail.com>
2025-06-28 22:59:50 +0000

    fix

--- downloadpjkt.sh
+++ downloadpjkt.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-git clone git@github.com:Rosenrotfan/shvirtd-example-python.git -d /opt/shvirtd-example-python
-cd /opt/shvirtd-example-python
-docker compose up -d
+git clone git@github.com:Rosenrotfan/shvirtd-example-python.git /opt/shvirtd-example-python && cd /opt/shvirtd-example-python && docker compose up -d
